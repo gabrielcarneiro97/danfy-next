f32feb1855b76901d96f4dfd1fdbf46c9d3d598c
Gabriel Carneiro <gabriel.carneiro97@live.com>
2020-07-13 10:27:02 -0300

    adicionei o prisma

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,6 +1,12 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { PrismaClient } from '@prisma/client';
+import { NextApiRequest, NextApiResponse } from 'next';
+// or const { PrismaClient } = require('@prisma/client')
 
-export default (req, res) => {
-  res.statusCode = 200
-  res.json({ name: 'John Doe' })
-}
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  const prisma = new PrismaClient();
+
+  const pessoa = await prisma.pessoa.findOne({ where: { cpfCnpj: '11009961000183' }, include: { endereco: true } });
+  res.statusCode = 200;
+  res.json(pessoa);
+};
