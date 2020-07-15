d8d48911d876270664126a75fa60168893c9bfe1
Gabriel Carneiro <gabriel.carneiro97@live.com>
2020-07-15 14:34:35 -0300

    fazendo alguns ajustes na rota de files, stress test

--- src/pages/api/file.ts
+++ src/pages/api/file.ts
@@ -4,13 +4,17 @@ import multer from 'multer';
 
 import runMiddleware from 'services/middlewere';
 import conversor from 'services/xml/conversor';
+import danfe from 'services/xml/danfe';
+import servico from 'services/xml/notaServico';
 
 const upload = multer();
 
 export default async (req : NextApiRequest & { file : any }, res : NextApiResponse) => {
   await runMiddleware(req, res, upload.single('file'));
 
-  console.log(conversor(req.file));
+  const obj = conversor(req.file);
+
+  console.log(servico.localizador.qualCidade(obj)(obj)[0].notaServico.chave);
 
   res.json('Hello');
 };
